ec9fd96ca3ffb724ece61a70ca99e77c2789a228
y-eon <67999440+y-eon@users.noreply.github.com>
2020-09-06 17:24:56 +0900

    4.3 뮤직비디오

---
+++ Python_algorithm/section3_03.py
@@ -0,0 +1,27 @@
+import sys
+#sys.stdin = open('input.txt','r')
+n, m = map(int, input().split())
+a = list(map(int,input().split()))
+
+def Count(capacity):
+    cnt = 1
+    sum = 0
+    for x in a:
+        if sum+x>capacity:
+            cnt += 1
+            sum = x
+        else:
+            sum += x
+    return cnt
+
+lt = 1
+rt = sum(a)
+res = 0
+while lt<=rt:
+    mid = (lt+rt)//2
+    if Count(mid)<=m:
+        res = mid
+        rt = mid - 1
+    else:
+        lt = mid + 1
+print(res)
